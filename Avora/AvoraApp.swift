c81ef71ca2ae9b94efeef0c8557f34d93c4d02d7
dqhieu <dqhieu13@gmail.com>
2026-06-26 21:52:40 +0700

    Initial Commit

---
+++ Avora/AvoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvoraApp.swift
+//  Avora
+//
+//  Created by Hieu Dinh on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct AvoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
