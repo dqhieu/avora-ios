43b3c06bc524ac1fd1599642e0044b326261de23
dqhieu <dqhieu13@gmail.com>
2026-06-28 21:16:11 +0700

    chore: init supabase local stack and core extensions

---
+++ supabase/migrations/000001_extensions.sql
@@ -0,0 +1,4 @@
+create extension if not exists pgtap with schema extensions;
+create extension if not exists pg_net with schema extensions;
+create extension if not exists pg_cron;
+create extension if not exists pgmq;
